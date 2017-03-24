1d69380f62bc36bb7cb44626e172203a439f7acc
Scott Grimes <scott-grimes@users.noreply.github.com>
2017-03-24 11:16:51 -0500

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-class client:
-    
-    def __init__(self,sock,name,salt,key):
-        self.socket = sock
-        self.name = name
-        self.salt = salt
-        self.key = key
-        
